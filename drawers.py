07e2623baedb2ea7c135f4444b040897be684616
Yuncong Zhang <yczhangsjtu@gmail.com>
2022-11-17 14:13:49 +0800

    Update

--- drawers.py
+++ drawers.py
@@ -28,7 +28,7 @@ class BoxDrawer(Drawer):
     """
     The LaTeX equations are smaller than expected.
     """
-    latex_scale_ratio = 0.6
+    latex_scale_ratio = 0.5
 
     if "scale" in obj:
       scale = float(obj["scale"])
