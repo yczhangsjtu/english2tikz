576da4d34daa4ddd30f7d1fd606ea7b0ae343f40
Yuncong Zhang <yczhangsjtu@gmail.com>
2022-11-25 21:25:08 +0800

    Update object_utils.py

--- gui/object_utils.py
+++ gui/object_utils.py
@@ -3,16 +3,16 @@ from english2tikz.utils import *
 
 
 def draw_border(obj):
-  return get_default(obj, "draw", False) or is_type(obj, "box")
+  return obj.get("draw", False) or is_type(obj, "box")
 
 
 def get_draw_color(obj):
-  return get_default(obj, "color", "black") if draw_border(obj) else ""
+  return obj.get("color", "black") if draw_border(obj) else ""
 
 
 def get_text_color(obj):
-  text_color = get_default(obj, "text.color")
-  text_color = get_default(obj, "color") if text_color is None else text_color
+  text_color = obj.get("text.color")
+  text_color = obj.get("color") if text_color is None else text_color
   text_color = "black" if text_color is None else text_color
   return text_color
 
@@ -37,7 +37,7 @@ def get_original_pos(obj, bounding_boxes, position=None):
     at_bounding_box = bounding_boxes[at]
     x, y = at_bounding_box.get_anchor_pos(direction_to_anchor(direction))
     dx, dy = direction_to_num(direction)
-    dist = get_default(obj, "distance", 1)
+    dist = obj.get("distance", 1)
     if isinstance(dist, str) and dist.find("and") >= 0:
       disty, distx = dist_to_num(*dist.split(".and."))
     else:
@@ -49,21 +49,21 @@ def get_original_pos(obj, bounding_boxes, position=None):
   if isinstance(at, str):
     at_bounding_box = bounding_boxes[at]
     return at_bounding_box.get_anchor_pos(
-        get_default(obj, "at.anchor", "center"))
+        obj.get("at.anchor", "center"))
   elif at["type"] == "coordinate":
-    return dist_to_num(get_default(at, "x", 0), get_default(at, "y", 0))
+    return dist_to_num(at.get("x", 0), at.get("y", 0))
   elif at["type"] == "intersection":
     x, _ = bounding_boxes[at["name1"]].get_anchor_pos(
-        get_default(at, "anchor1", "center"))
+        at.get("anchor1", "center"))
     _, y = bounding_boxes[at["name2"]].get_anchor_pos(
-        get_default(at, "anchor2", "center"))
+        at.get("anchor2", "center"))
     return x, y
   else:
     raise ValueError(f"Unsupported at {at}")
 
 
 def get_rounded_corners(obj, default):
-  rounded_corners = get_default(obj, "rounded.corners")
+  rounded_corners = obj.get("rounded.corners")
   if rounded_corners is True:
     return default
   if rounded_corners is not None:
